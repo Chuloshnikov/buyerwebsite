0a0ae1ea9501a25973362a2d47440c417b86adf9
MaxChuloshnikov <Maks447@ukr.net>
2023-03-29 18:58:05 +0300

    Navbar added

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,7 @@ import logo from "../assets/images/logo.png" ;
 const Navbar = () => {
   return (
         <div className='w-full bg-white text-black'>
-           <div className='max-w-container mx-auto flex justify-between'>
+           <div className='max-w-container mx-auto flex justify-between items-center border-b-[1px] border-b-gray-300'>
               <div className='flex items-center gap-3 text-3xl text-[#F77628] py-2'>
                 <Image className='w-10 h-10 rounded-full border-[3px] border-[#F77628]' src={logo} alt="logo"/>
                 <p>Buyer Anastasia</p>
