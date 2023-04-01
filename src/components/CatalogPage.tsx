264827a41b4909f40a5034e125b6f461ba856030
MaxChuloshnikov <Maks447@ukr.net>
2023-04-01 18:13:42 +0300

    ProductPage styles Done

--- src/components/CatalogPage.tsx
+++ src/components/CatalogPage.tsx
@@ -1,14 +1,54 @@
 import React from 'react';
+import { Item } from "../../type";
+import Image from "next/image";
+import { GoPlus } from 'react-icons/go';
+import { isTemplateSpan } from 'typescript';
 
 
 
 
-const CatalogPage = ({ProductData}: any) => {
+const CatalogPage = ({ productData }: any) => {
   return (
-    <div className='py-6 px-4 grid grid-cols-4 gap-4'>
-        {ProductData.map(item) => (
-            <div></div>
-        )
+    <div className='py-6 px-4 grid xs:grid-cols-1 md:grid-cols-2 mdl:grid-cols-3 lgl:grid-cols-4  gap-4'>
+        {productData.map((item: Item) => (
+            <div key={item._id} className="border-[1px] border-gray-200 mb-6 group">
+              <div className="w-full h-[350px] overflow-hidden p-1">
+                <Image 
+                  className='w-full h-full object-contain scale-100 group-hover:scale-105 duration-300'
+                  width={300} 
+                  height={250} 
+                  src={item.img} 
+                  alt="itemImage"/>
+              </div>
+                {/* Description */}
+                <div className='px-2 py-4 flex flex-col justify-center'>
+                  <div className='text-base text-white flex justify-between'>
+                    <button className=' p-2 rounded-lg bg-orange-400 flex items-center justify-center gap-1
+                    hover:scale-105 hover:bg-orange-500 duration-300'
+                    >
+                      <span><GoPlus/></span>
+                      У кошик
+                    </button>
+                    <button className='p-2 rounded-lg bg-orange-400 flex items-center justify-center
+                    hover:scale-105 hover:bg-orange-500 duration-300'
+                    >
+                      Детальніше
+                    </button>
+                  </div>
+                  <div className='flex items-center gap-3 pt-2'>
+                    <p className='text-lg font-semibold text-green-500'>
+                      Ціна: {item.price} грн.
+                    </p>
+                    { item.oldPrice ? <p className='text-gray-500 line-through decoration-[1px]'>{item.oldPrice} грн.</p> : ""}
+                  </div>
+                  <div className='flex justify-between py-2'>
+                    <p className='text-lg font-bold text-gray-600'>{item.title.substring(0, 25)}</p>
+                    <p className='text-sm font-semibold text-gray-500 mr-2'>{item.brand}</p>
+                  </div>
+                  <p className='text-base text-zinc-500'>{item.description.substring(0, 80)}...</p>
+                </div>
+            </div>
+          ))
         }  
     </div>
   )
--- type.d.ts
+++ type.d.ts
@@ -8,4 +8,18 @@ export interface Product {
     sizes: string;
     img: string;
     category: string;
+    quantity: number;
 }[];
+
+export interface Item {
+    _id: number;
+    title: string;
+    manufacturer: string;
+    description: string;
+    oldPrice: number;
+    price: number;
+    sizes: string;
+    img: string;
+    category: string;
+    quantity: number;
+};
