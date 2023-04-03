6745a274c69009629ed3cad3cee0110eff813c2a
MaxChuloshnikov <Maks447@ukr.net>
2023-04-03 12:14:27 +0300

    ProductPage Adaptive Done

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
+import { GoPlus } from 'react-icons/go';
 
 const ProductDetails = () => {
   const router = useRouter();
@@ -18,19 +19,19 @@ const ProductDetails = () => {
   return (
     <div className='w-full bg-white text-gray-800 px-4'>
        <div className='max-w-contentContainer mx-auto flex items-center py-4 xs:flex-col lg:flex-row'>
-          <div className='w-2/3 h-full flex items-center justify-center overflow-hidden relative'>
+          <div className='w-2/3 h-full xs:w-full flex items-center justify-center overflow-hidden relative'>
           <img
                 src={product.img}
                 alt="productImg"
-                className='w-[70%] transform-origin-top-left cursor-move duration-500'
+                className='w-[80%] transform-origin-top-left cursor-move duration-500'
                 />
             
           </div>
-          <div className='w-1/3 h-full flex flex-col gap-2'>
+          <div className='w-1/3 h-full xs:w-full flex flex-col gap-2'>
             <div>
               {product.quantity <= 0 ? 
-              (<div className='p-1 bg-red-400 rounded-md'>
-                  <p className='py-2'>Нажаль у данний момент цього товару нема в наявності</p>
+              (<div className='xs:p-0 mdl:p-1 bg-red-400 rounded-md'>
+                  <p className='mdl:py-2 xs:p-1 md:p-0 text-center'>Нажаль у данний момент цього товару нема в наявності</p>
                 </div>) :
               ("")
               }
@@ -40,7 +41,7 @@ const ProductDetails = () => {
                 <p className='text-lg font-semibold'>{product.brand}</p>
             </div> 
             <div className='p-2'>
-                <p className='text-lg font-base'>Категорія: {product.category}</p>
+                <p className='text-sm font-base'>Категорія: {product.category}</p>
             </div>
             <div className='p-2'>
                 <p className='text-sm font-base'>Poзмір: {product.sizes}</p>
@@ -59,12 +60,13 @@ const ProductDetails = () => {
                 </p>
             </div>
             <div className='p-2'>
-                <p className='text-lg font-base'>{product.description}</p>
+                <p className='text-sm font-base'>{product.description}</p>
             </div>
             <div className='p-2'>
-                <button className='py-3 px-2 bg-orange-400 text-white rounded-md 
+                <button className='flex items-center gap-1 py-3 px-2 bg-orange-400 text-white rounded-md 
                 hover:scale-105 hover:bg-orange-500 duration-300'
                 >
+                  <span><GoPlus/></span>
                     Додати у кошик
                 </button>
             </div>
