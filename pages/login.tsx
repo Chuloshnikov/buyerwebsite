70013f6cdc451d0938b21026845f994e13ccefac
MaxChuloshnikov <Maks447@ukr.net>
2023-05-17 20:16:15 +0300

    Login page stylization done

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import CredentialsForm from '../components/CredentialsForm';
+import Image from 'next/image';
 import Link from 'next/link';
 import Head from 'next/head';
+import {
+    googleLogo,
+    facebookLogo,
+    instagramLogo
+} from '../assets/images/index';
+import { HiAtSymbol, HiFingerPrint } from 'react-icons/hi';
+import styles from '../styles/CredentialsForm.module.css';
 
 const Login = () => {
+
+    const [show, setShow] = useState(false);
+
   return (
         <CredentialsForm>
             <Head>
@@ -13,45 +24,72 @@ const Login = () => {
                 <link rel="icon" href="/buyerfavicon.ico" />
             </Head>
             <section className='mx-auto flex flex-col'>
-                <div>
-                    <Link href="login">To Login</Link>
-                    <Link href="register">To Register</Link>
-                </div>
-                <h1>Login</h1>
+                <h1>Реєстрація</h1>
                 <form className='flex flex-col gap-2 mt-5'>
-                    <div className='input-item'>
+                    <div className='flex justify-center'>
                         <input
-                        className='border-b-[1px] border-b-gray-400'
+                        className='border-b-[1px] border-b-gray-400 text-gray-700'
                         type="email"
                         name="email"
                         placeholder='Email'
-                        >
-                        </input>
+                        />
+                        <HiAtSymbol 
+                        size={20} 
+                        className="text-gray-400 -ml-6 mt-1"
+                        />
                     </div>
-                    <div className='input-item'>
+                    <div className='flex justify-center'>
                         <input
-                        className='border-b-[1px] border-b-gray-400'
-                        type="password"
+                        className='border-b-[1px] border-b-gray-400 text-gray-700'
+                        type={`${show ? "text" : "password"}`}
                         name="password"
                         placeholder='Password'
-                        >
-                        </input>
+                        />
+                        <HiFingerPrint 
+                        onClick={() => setShow(!show)}
+                        size={20} 
+                        className="text-gray-400 -ml-6 mt-1"
+                        />
                     </div>
-                    <div className='flex flex-col gap-2'>
-                        <button>
-                            Log in
+                    <div className='flex flex-col gap-2 mt-2'>
+                        <button className='bg-green-600 text-white 
+                        font-semibold text-center mx-12 rounded-md py-1
+                        hover:scale-105 duration-300'
+                        >
+                            Реєстрація
                         </button>
-                        <button>
-                            Sign in with Google
+                        <button 
+                        className='text-gray-600 flex gap-1 items-center 
+                        justify-center border-2 
+                        py-1 mx-12 rounded-md hover:scale-105 duration-300'
+                        >
+                            <span>Реєстрація з Google</span>
+                            <Image src={googleLogo} width={14} height={14}/>
                         </button>
-                        <button>
-                            Sign in with Instagram
+                        <button 
+                        className='text-gray-600 flex gap-1 items-center 
+                        justify-center border-2
+                        py-1 mx-12 rounded-md hover:scale-105 duration-300'
+                        >
+                            <span>Реєстрація з Instagram</span>
+                            <Image src={instagramLogo} width={14} height={14}/>
                         </button>
-                        <button>
-                            Sign in with Facebook
+                        <button 
+                        className='text-gray-600 flex gap-1 items-center 
+                        justify-center border-2
+                        py-1 mx-12 rounded-md hover:scale-105 duration-300'
+                        >
+                            <span>Реєстрація з Facebook</span>
+                            <Image src={facebookLogo} width={14} height={14}/>
                         </button>
                     </div>
                 </form>
+                <p className='mt-10 text-xs font-semibold text-center text-gray-400'>
+                    Ви ще не зареєструвались?{' '}
+                    <Link className='text-blue-600 hover:underline duration-200' href={'/register'}>
+                        Реєстрація
+                    </Link>
+                </p>
             </section>
         </CredentialsForm>
   )
--- pages/register.tsx
+++ pages/register.tsx
@@ -7,16 +7,12 @@ const Register = () => {
   return (
         <CredentialsForm>
             <Head>
-                <title>Register</title>
+                <title>Регістрація</title>
                 <meta name="Баєр Анастасія" content="Tвій найкращий баєр" />
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <link rel="icon" href="/buyerfavicon.ico" />
             </Head>
             <section className='mx-auto flex flex-col'>
-                <div>
-                    <Link href="login">To Login</Link>
-                    <Link href="register">To Register</Link>
-                </div>
                 <h1>Register</h1>
             </section>
         </CredentialsForm>
