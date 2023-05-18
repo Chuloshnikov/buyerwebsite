e1a969be7ba7e072ca936152f527e2d3bf7f741f
MaxChuloshnikov <Maks447@ukr.net>
2023-05-18 13:08:02 +0300

    Registr page stylization done

--- pages/register.tsx
+++ pages/register.tsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import CredentialsForm from '../components/CredentialsForm';
 import Head from 'next/head';
 import Link from 'next/link';
 
+import { HiAtSymbol, HiFingerPrint, HiOutlineUser } from 'react-icons/hi2';
+import styles from '../styles/CredentialsForm.module.css';
+
 const Register = () => {
+    const [show, setShow] = useState({password: false, cpassword: false});
+
+
   return (
         <CredentialsForm>
             <Head>
@@ -13,7 +19,73 @@ const Register = () => {
                 <link rel="icon" href="/buyerfavicon.ico" />
             </Head>
             <section className='mx-auto flex flex-col'>
-                <h1>Register</h1>
+            <h1>Регістрація</h1>
+                <form className='flex flex-col gap-2 mt-5'>
+                <div className='flex justify-center'>
+                        <input
+                        className='border-b-[1px] border-b-gray-400 text-gray-700'
+                        type="text"
+                        name="Username"
+                        placeholder='Username'
+                        />
+                        <HiOutlineUser
+                        size={20} 
+                        className="text-gray-400 -ml-6 mt-1"
+                        />
+                    </div>
+                    <div className='flex justify-center'>
+                        <input
+                        className='border-b-[1px] border-b-gray-400 text-gray-700'
+                        type="email"
+                        name="email"
+                        placeholder='Email'
+                        />
+                        <HiAtSymbol
+                        size={20} 
+                        className="text-gray-400 -ml-6 mt-1"
+                        />
+                    </div>
+                    <div className='flex justify-center'>
+                        <input
+                        className='border-b-[1px] border-b-gray-400 text-gray-700'
+                        type={`${show.password ? "text" : "password"}`}
+                        name="password"
+                        placeholder='Password'
+                        />
+                        <HiFingerPrint
+                        onClick={() => setShow({ ...show, password: !show.password })}
+                        size={20} 
+                        className="text-gray-400 -ml-6 mt-1"
+                        />
+                    </div>
+                    <div className='flex justify-center'>
+                        <input
+                        className='border-b-[1px] border-b-gray-400 text-gray-700'
+                        type={`${show.cpassword ? "text" : "password"}`}
+                        name="password"
+                        placeholder='Confirm Password'
+                        />
+                        <HiFingerPrint
+                        onClick={() => setShow({ ...show, cpassword: !show.cpassword })}
+                        size={20} 
+                        className="text-gray-400 -ml-6 mt-1"
+                        />
+                    </div>
+                    <div className='flex flex-col gap-2 mt-2'>
+                        <button className='bg-green-600 text-white 
+                        font-semibold text-center mx-12 rounded-md py-1
+                        hover:scale-105 duration-300'
+                        >
+                            Реєстрація
+                        </button>
+                    </div>
+                </form>
+                <p className='mt-10 text-xs font-semibold text-center text-gray-400'>
+                    Ви вже зареэструвались?{' '}
+                    <Link className='text-blue-600 hover:underline duration-200' href={'/login'}>
+                        Логін
+                    </Link>
+                </p>
             </section>
         </CredentialsForm>
   )
