7ce6e5891d6687cc1cca3c78a6e95adb6403b7cb
MaxChuloshnikov <Maks447@ukr.net>
2023-06-05 19:43:29 +0300

    Login page gif added
    3d cansoled

--- components/CredentialsForm.tsx
+++ components/CredentialsForm.tsx
@@ -1,22 +1,22 @@
-import styles from '../styles/CredentialsForm.module.css';
+import Image from "next/image";
+import img from "../assets/icons/registerImg/4.gif"
 
+const CredentialsForm = ({ children }) => {
 
-const CredentialsForm = ({children}) => {
   return (
-  
-        <div className='flex py-10'>
-            <div className='m-auto bg-white rounded-md flex'>
-                <div className='shadow-bannerShadow xs:hidden mdl:block'>
-                    <div className='w-[300px] h-[400px]'>
-                        Images
-                    </div>
-                </div>
-                <div className='text-center py-10 w-[300px] h-[400px] shadow-bannerShadow'>
-                    {children}
-                </div>
-            </div>
+    <div className='flex py-10'>
+      <div className='m-auto bg-white rounded-md flex'>
+        <div className='shadow-bannerShadow xs:hidden mdl:block'>
+          <div className='w-[300px] h-[400px]'>
+            <Image src={img} className="w-[300px] h-[400px]"/>
+          </div>
         </div>
-  )
-}
+        <div className='text-center py-10 w-[300px] h-[400px] shadow-bannerShadow'>
+          {children}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default CredentialsForm;
